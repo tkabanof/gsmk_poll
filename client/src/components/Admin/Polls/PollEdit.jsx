22c20cdc93593716a24a9a262b91bc9688374329
Timofey <crysistima@mail.ru>
2021-05-19 16:24:01 +0300

    Удалось прочитать csv и спарсить в массив обьектов

--- client/src/components/Admin/Polls/PollEdit.jsx
+++ client/src/components/Admin/Polls/PollEdit.jsx
@@ -41,22 +41,61 @@ const PollEdit = (props) => {
         props.setIsModalVisible(false)
     };
 
+    function csvToArray(str, delimiter = ",") {
+        // slice from start of text to the first \n index
+        // use split to create an array from string by delimiter
+        const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+
+        // slice from \n index + 1 to the end of the text
+        // use split to create an array of each csv value row
+        const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+
+        // Map the rows
+        // split values from each row into an array
+        // use headers.reduce to create an object
+        // object properties derived from headers:values
+        // the object passed as an element of the array
+        const arr = rows.map(function (row) {
+            const values = row.split(delimiter);
+            const el = headers.reduce(function (object, header, index) {
+                object[header] = values[index];
+                return object;
+            }, {});
+            return el;
+        });
+
+        // return the array
+        return arr;
+    }
+
     const handleChange = (e) => {
 
         let file = e.target.files[0]
-        console.log(file.size)
+        console.log(file.name)
 
         const reader = new FileReader();
-
-        reader.readAsText(file);
-
-        reader.onerror = () => {
-            console.log('Ошибка чтения файла')
+        reader.onprogress = (event) => {
+            console.log(event.loaded + '/' + event.total)
         }
-
+        reader.onerror = ()=>{
+            console.log('Ошибка чтения csv')
+        }
         reader.onload = () => {
-            console.log(reader.result)
+            const text = reader.result
+            const data = csvToArray(text)
+            console.log(data)
         }
+        reader.readAsText(file)
+
+
+        // const reader = new FileReader();
+        // reader.readAsText(file);
+        // reader.onerror = () => {
+        //     console.log('Ошибка чтения файла')
+        // }
+        // reader.onload = () => {
+        //     console.log(reader.result)
+        // }
 
     }
     return (
