afbf34f8678c1e904ee4e1f92baff42b3590bece
Timofey <crysistima@mail.ru>
2021-05-27 12:50:03 +0300

    Рефактор. Меню выбора звонков Фикс

--- client/src/components/Call/CallInfoCard.jsx
+++ client/src/components/Call/CallInfoCard.jsx
@@ -4,11 +4,16 @@ import {useEffect, useState} from "react";
 
 const CallInfoCard = (props) => {
 
-
     let [data, setData] = useState(1)
-    let [sortParam, setSortParam] = useState("Сортировка")
-    let [values, setValues] = useState([])
+    let [sortParam, setSortParam] = useState("Признак")
+    let [values, setValues] = useState(['1', '2'])
+    let [value, setValue] = useState('Значение')
 
+    const setSortParams = (value) => {
+        setSortParam(value)
+        setValues(data[sortParam])
+
+    }
     const onLoad = async (id) => {
         return await pollApi.getPollFieldValueData(id)
     }
@@ -16,39 +21,53 @@ const CallInfoCard = (props) => {
     useEffect(() => {
         onLoad(props.id).then((r) => {
             setData(r.data.keyValue)
+        }).catch(() => {
+            console.log('Ошибка')
         })
     }, [])
 
-    useEffect(()=> {
-        setValues(data[sortParam])
-    },[sortParam])
-
     let menu = (
         <Menu>
-            {Object.keys(data).map((p)=>{
-               return(
-                   <Menu.Item>
-                       <a target="_blank"
-                          onClick={(event)=>setSortParam(event.target.text)}
-                          key = {p}
-                       >
-                           {p}
-                       </a>
-                   </Menu.Item>
-               )
+            {Object.keys(data).map((p) => {
+                return (
+                    <Menu.Item>
+                        <a target="_blank"
+                           onClick={(event) => setSortParams(event.target.text)}
+                           key={p}
+                        >
+                            {p}
+                        </a>
+                    </Menu.Item>
+                )
             })}
         </Menu>
-    );
+    )
+    let menu2 = (
+        <Menu>
+            {values.map((p) => {
+                return (
+                    <Menu.Item>
+                        <a target="_blank"
+                           onClick={(event) => setValue(event.target.text)}
+                           key={p}
+                        >
+                            {p}
+                        </a>
+                    </Menu.Item>
+                )
+            })}
+        </Menu>
+    )
     return (
         <Card title={props.name}
-              //extra={<a href="#">More</a>}
+            //extra={<a href="#">More</a>}
               style={{width: 700}}>
             <Dropdown overlay={menu} placement="bottomLeft">
                 <Button>{sortParam}</Button>
             </Dropdown>
             <a> </a>
-            <Dropdown overlay={menu} placement="bottomLeft">
-                <Button>{sortParam}</Button>
+            <Dropdown overlay={menu2} placement="bottomLeft">
+                <Button>{value}</Button>
             </Dropdown>
         </Card>
     )
