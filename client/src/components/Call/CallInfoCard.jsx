9028e6498b65851a83bd2fcec9b6eeabeb9e31e7
Timofey <crysistima@mail.ru>
2021-06-04 10:39:30 +0300

    Выбор звонков по всем МО. Фиск. Добавил сортировку в меню выбора

--- client/src/components/Call/CallInfoCard.jsx
+++ client/src/components/Call/CallInfoCard.jsx
@@ -1,8 +1,9 @@
-import {Button, Card, Dropdown, Menu} from "antd";
+import {Button, Card, Dropdown, Menu, Select} from "antd";
 import {pollApi} from "../../Api/Api";
 import {useEffect, useState} from "react";
 import {useHistory} from "react-router-dom";
 import {CALL_ROUTE_DETAIL} from "../utils/consts";
+import {Option} from "antd/es/mentions";
 
 const CallInfoCard = (props) => {
 
@@ -62,14 +63,32 @@ const CallInfoCard = (props) => {
         </Menu>
     )
 
+    let menu2 = (<Select defaultValue="Все МО"
+                         showSearch
+                         style={{ width: 620 }}
+                         onChange={setMO}>
+            <Option value="ALL">Все МО</Option>
+            {data && data.map((p) => {
+                return (
+                    <Option value={p}>
+                        {p}
+                    </Option>
+                )
+            })}
+        </Select>
+    )
+
     return (
         <Card title={name}
             //extra={<a href="#">More</a>}
               style={{width: 700}}>
 
-            <Dropdown overlay={menu} placement="bottomLeft">
-                <Button>Выбор МО</Button>
-            </Dropdown>
+            {/*<Dropdown overlay={menu} placement="bottomLeft">*/}
+            {/*    <Button>Выбор МО</Button>*/}
+            {/*</Dropdown>*/}
+
+                {menu2}
+
         </Card>
     )
 }
